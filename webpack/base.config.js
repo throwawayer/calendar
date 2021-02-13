1485dfbe8fb119726081c3b687d2598d6154d957
throwawayer <zurdig@gmail.com>
2021-02-14 01:29:54 +0200

    CLDR-1: fixes/refactors - refactored webpack - fixed production build/run - updated package for security fix - updated README.md

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,20 +1,27 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const { paths } = require('./vars');
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const plugins = [
   new HtmlWebpackPlugin({
     template: path.resolve('index.html'),
     favicon: path.resolve('src', 'assets', 'images', 'favicon.ico'),
+    filename: 'index.html',
+    inject: true,
   }),
 ];
 
-if (process.env.NODE_ENV === 'development') {
-  plugins.push(new CleanWebpackPlugin.CleanWebpackPlugin());
+if (isDevelopment) {
   plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+    new CleanWebpackPlugin.CleanWebpackPlugin(),
     new CircularDependencyPlugin({
       exclude: /node_modules/,
       failOnError: false,
@@ -22,12 +29,14 @@ if (process.env.NODE_ENV === 'development') {
       cwd: process.cwd(),
     }),
   );
-  plugins.push(new webpack.HotModuleReplacementPlugin());
-  plugins.push(new webpack.LoaderOptionsPlugin({ debug: true }));
 } else {
   plugins.push(
     new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
     new webpack.ids.HashedModuleIdsPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new webpack.ids.DeterministicModuleIdsPlugin({
+      maxLength: 8,
+    }),
   );
 }
 
@@ -38,7 +47,6 @@ module.exports = {
   output: {
     path: paths.output,
     filename: 'bundle.js',
-    publicPath: '/',
   },
   plugins,
   module: {
